1bae1139de473aa067468f1d68b6b529e0cda6bd
krishpranav <krisna.pranav@gmail.com>
2021-06-25 19:51:32 +0530

    [+] check [+]

--- spec/streaming_api_spec.rb
+++ spec/streaming_api_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Rbshodan::API::Streaming do
         sleep 3
     end
 
-    
+    describe '#banners' do
+        def check
+            @client.banners(limit:  1) do |banner|
+                expect(banner).to be_a(Hash)
+            end
+        end
+        
