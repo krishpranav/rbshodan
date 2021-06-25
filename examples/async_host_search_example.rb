3656c55f2b3371fc1592c3c3f86ff09472f224b0
krishpranav <krisna.pranav@gmail.com>
2021-06-25 18:56:54 +0530

    [+] webserver [+]

--- examples/async_host_search_example.rb
+++ examples/async_host_search_example.rb
@@ -7,3 +7,10 @@ client = Rbshodan.client.new
 webservers = ['apache', 'nginx', 'caddy', 'lighttpd', 'cherokee']
 
 started = Time.now.sec
+webservers.each do |webserver|
+    client.rest_api.host_search(webserver)
+    puts webserver
+end
+
+puts "Sequential took #{Time.now.sec - started} seconds"
+
