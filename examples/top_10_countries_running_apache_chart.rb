ff3fac6d61eefbcee1b695677fcd1bf92dff4bd3
krishpranav <krisna.pranav@gmail.com>
2021-06-25 19:06:28 +0530

    [+] top 10 module [+]

--- examples/top_10_countries_running_apache_chart.rb
+++ examples/top_10_countries_running_apache_chart.rb
@@ -1,5 +1,20 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'shodanz'
+require 'rbshodan'
 require 'command_lion'
 require 'yaml'
 require 'pry'
+
+
+module Top10
+
+    @rest_api = Rbshodan.api.rest.new
+
+    def self.check(product)
+        begin
+            @rest_api.host_count(product: product, facets: { country: 10 })["facets"]["country"].collect { |x| x.values }.to_h.invert
+        rescue
+            puts "Unable to succesfully check the shodan API."
+            exit 1
+        end
+    end
+end
