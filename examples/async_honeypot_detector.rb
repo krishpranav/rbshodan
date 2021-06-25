bff15d0dc99dc0749e9b022fa2bb398ce258bcdf
krishpranav <krisna.pranav@gmail.com>
2021-06-25 18:54:06 +0530

    [+] async honeypot detector [+]

--- examples/async_honeypot_detector.rb
+++ examples/async_honeypot_detector.rb
@@ -3,3 +3,19 @@ require 'async'
 require 'rbshodan'
 
 client = Rbshodan.client.new
+
+
+client.streaming_api.banners do |banner|
+    if ip = banner['ip_str']
+        Async do
+            score = client.rest_api.honeypot_score(ip).wait
+            puts "#{ip} has a #{score * 100}% chance of being a honeypot"
+        rescue Rbshodan::Errors::RateLimited
+            sleep rand
+            retry
+        rescue StandartError
+            next
+        end
+    end
+end
+
