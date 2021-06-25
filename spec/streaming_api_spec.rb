9fd089d1332c863bd06e6d14ef649e00a476e332
krishpranav <krisna.pranav@gmail.com>
2021-06-25 19:52:01 +0530

    [+] streaming api spec [+]

--- spec/streaming_api_spec.rb
+++ spec/streaming_api_spec.rb
@@ -1,18 +1,32 @@
 require "spec_helper"
 
 RSpec.describe Rbshodan::API::Streaming do
-    before do
-        @client = Rbshodan.api.Streaming.new
+  before do
+    @client = Rbshodan.api.streaming.new
+  end
+
+  before(:each) do
+    # try to avoid rate limit
+    sleep 1
+  end
+
+  describe '#banners' do
+    def check
+      @client.banners(limit: 1) do |banner|
+        expect(banner).to be_a(Hash)
+      end
     end
 
-    before(:each) do
-        sleep 3
-    end
+    describe 'stream any banner data Shodan collects' do
+      it 'works synchronously' do
+        check
+      end
 
-    describe '#banners' do
-        def check
-            @client.banners(limit:  1) do |banner|
-                expect(banner).to be_a(Hash)
-            end
+      it 'works asynchronously' do
+        Async do
+          check
         end
-        
+      end
+    end
+  end
+end
