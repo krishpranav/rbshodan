279122a4c1edba6a0482455931646bae820580b0
krishpranav <krisna.pranav@gmail.com>
2021-06-25 19:45:39 +0530

    [+] Spec helper [+]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,14 @@ require "async/rspec"
 require "rbshodan"
 require "pry"
 require "readline"
+
+
+RSpec.configure do |config|
+    config.example_status_persistence_file_path = ".rspec_status"
+
+    config.disable_monkey_patching!
+
+    config.expect_with :rspec do |c|
+        c.syntax = :expect
+    end
+end
