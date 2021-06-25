764fb30551ebe4b9819a9d995ac0f56b5d62debb
krishpranav <krisna.pranav@gmail.com>
2021-06-25 18:59:58 +0530

    [+] bindings [+]

--- examples/async_stream_example.rb
+++ examples/async_stream_example.rb
@@ -12,3 +12,12 @@ ports    = [21, 22, 80, 443]
 services = ['ftp', 'ssh', 'http', 'https']
 
 ports_with_service_names = ports.zip(services)
+
+Async do
+    ports_with_service_names.each do |port, service|
+        client.streaming_api.banners_on_port(port) do |banner|
+            if ip = banner['ip_str']
+                Async do
+                    resp = client.rest_api.honeypot_score(ip).wait
+                    binding.pry if resp.nil?
+                    
