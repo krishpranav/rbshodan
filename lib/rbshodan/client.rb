5730a8474712ab0c7be365869a2bb1c1ccf93663
krishpranav <krisna.pranav@gmail.com>
2021-06-26 09:40:14 +0530

    [+] initialize [+]

--- lib/rbshodan/client.rb
+++ lib/rbshodan/client.rb
@@ -4,3 +4,12 @@ module Rbshodan
         attr_reader :rest_api
         attr_reader :streaming_api
         attr_reader :exploits_api
+
+        def initialize(key: ENV['SHODAN_API_KEY'])
+            raise Rbshodan::Errors::NoAPI if key.nil?
+
+            @rest_api       = Rbshodan.api.rest.new(key: key)
+            @streaming_api  = Rbshodan.api.streaming.new(key: key)
+            @exploits_api   = Rbshodan.api.exploits.new(key: key)
+        end
+        
