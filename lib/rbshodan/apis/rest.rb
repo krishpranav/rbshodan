a0350422b02e1f60e82326267b783ecce12ea56a
krishpranav <krisna.pranav@gmail.com>
2021-06-26 09:10:16 +0530

    [+] initialize [+]

--- lib/rbshodan/apis/rest.rb
+++ lib/rbshodan/apis/rest.rb
@@ -10,3 +10,10 @@ module Rbshodan
 
             URL = 'https://api.shodan.io'
 
+            def initialize(key: ENV['SHODAN_API_KEY'])
+                @url    = URL
+                @client = Async::HTTP::Client.new(Async::HTTP::Endpoint.parse(@url))
+                self.key = key
+
+                warn 'No key has been found or provided!' unless key?
+            end
