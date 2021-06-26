a760f6f0cee9e3e13b386c9cb3079fd33851416a
krishpranav <krisna.pranav@gmail.com>
2021-06-26 09:38:27 +0530

    [+] api [+]

--- lib/rbshodan/api.rb
+++ lib/rbshodan/api.rb
@@ -1,3 +1,23 @@
 require_relative 'apis/rest.rb'
 require_relative 'apis/streaming.rb'
 require_relative 'apis/exploits.rb'
+
+
+module Rbshodan
+    
+    module API
+
+        def self.rest
+            REST
+        end
+
+
+        def self.streaming
+            Streaming
+        end
+
+        def self.exploits
+            Exploits
+        end
+    end
+end
