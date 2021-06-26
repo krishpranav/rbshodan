44413609b495273de474894321e13b5e1bde567b
krishpranav <krisna.pranav@gmail.com>
2021-06-26 09:43:00 +0530

    [+] errors [+]

--- lib/rbshodan/errors.rb
+++ lib/rbshodan/errors.rb
@@ -1,8 +1,40 @@
-module Rbshodan
+module Shodanz
     module Errors
-        class RateLimited < StandardError
-            def initialize(msg = 'Request rate limit reached (1 request/ second). Please wait a second before trying again and slow down your API calls.')
-                super
-            end
+      class RateLimited < StandardError
+        def initialize(msg = 'Request rate limit reached (1 request/ second). Please wait a second before trying again and slow down your API calls.')
+          super
         end
-        
+      end
+  
+      class NoInformation < StandardError
+        def initialize(msg = 'No information available.')
+          super
+        end
+      end
+  
+      class NoAPIKey < StandardError
+        def initialize(msg = 'No API key has been found or provided! ( setup your SHODAN_API_KEY environment variable )')
+          super
+        end
+      end
+  
+      class NoQuery < StandardError
+        def initialize(msg = 'Empty search query.')
+          super
+        end
+      end
+  
+      class AccessDenied < StandardError
+        def initialize(msg = 'Shodan subscription doesn\'t support action, check API permissions!')
+          super
+        end
+      end
+  
+      class InvalidKey < StandardError
+        def initialize(msg = 'Invalid API key used, or none given!')
+          super
+        end
+      end
+    end
+  end
+  
