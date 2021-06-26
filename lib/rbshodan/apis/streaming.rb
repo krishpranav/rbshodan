ae9f9ade1721e6e40f65e89515ceb39262f65153
krishpranav <krisna.pranav@gmail.com>
2021-06-26 09:30:07 +0530

    [+] streaming [+]

--- lib/rbshodan/apis/streaming.rb
+++ lib/rbshodan/apis/streaming.rb
@@ -1,24 +1,78 @@
 require_relative 'utils.rb'
 
-module Rbshodan
-    module API
 
-        class Streaming
-            include Rbshodan::API::Utils
+module Shodanz
+  module API
 
-            attr_accessor :key
+    class Streaming
+      include Shodanz::API::Utils
 
-            URL = 'https://stream.shodan.io'
+      attr_accessor :key
 
-            def initialize(key: ENV['SHODAN_API_KEY'])
-                @url           =   URL
-                @client        =   Async::HTTP::Client.new(Async::HTTP::Endpoint.parse(@url))
-                self.key       = key
+      URL = 'https://stream.shodan.io/'
 
-                warn 'No key has been found or provided!!!' unless key?
-            end
+      def initialize(key: ENV['SHODAN_API_KEY'])
+        @url      = URL
+        @client   = Async::HTTP::Client.new(Async::HTTP::Endpoint.parse(@url))
+        self.key  = key
 
-            def key?
-                return true if @key; false
-            end
-            
+        warn 'No key has been found or provided!' unless key?
+      end
+
+      def key?
+        return true if @key; false
+
+      end
+
+      def banners(**params)
+        slurp_stream('shodan/banners', **params) do |data|
+          yield data
+        end
+      end
+
+
+      def banners_within_asns(*asns, **params)
+        slurp_stream("shodan/asn/#{asns.join(',')}", **params) do |data|
+          yield data
+        end
+      end
+
+      def banners_within_asn(param)
+        banners_within_asns(param) do |data|
+          yield data
+        end
+      end
+
+      def banners_within_countries(*params)
+        slurp_stream("shodan/countries/#{params.join(',')}") do |data|
+          yield data
+        end
+      end
+
+      def banners_on_ports(*params)
+        slurp_stream("shodan/ports/#{params.join(',')}") do |data|
+          yield data
+        end
+      end
+
+    
+      def banners_on_port(param)
+        banners_on_ports(param) do |data|
+          yield data
+        end
+      end
+
+      def alerts
+        slurp_stream('alert') do |data|
+          yield data
+        end
+      end
+
+      def alert(id)
+        slurp_stream("alert/#{id}") do |data|
+          yield data
+        end
+      end
+    end
+  end
+end
